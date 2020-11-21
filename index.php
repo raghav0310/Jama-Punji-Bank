e1d238eb28e2109ebb6d976133eb419632a1aa6d
Raghav Sharma <42476309+raghav0310@users.noreply.github.com>
2020-11-22 01:19:17 +0530

    Uploaded Initially

---
+++ index.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+    <title>Basic Banking System</title>
+    <link rel="stylesheet" href="button.css">
+	<link rel="stylesheet" type="text/css" href="jamapunjihome.css">
+	<style>
+	button{
+		background-color:#8cbed6;
+		  border: none;
+  color: black;
+  padding: 30px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  cursor: pointer;
+  font-family: "Lucida Console", Courier, monospace;
+		border-radius: 12px;
+	}
+	tr{
+		text-align:center;
+	}
+	body
+	{
+		text-align:center;
+	}
+		body{
+		background-image:url("bank11.jpeg");
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	</style>
+	<style>
+.button {
+  border: none;
+  color: white;
+  padding: 30px 50px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {background-color: #fff;} 
+.button2 {background-color: #fff;} 
+</style>
+</head>
+<body >
+<header>	
+		<div class="main">
+			<ul>
+				<li><a href="jamapunjihome.html"> Home </a></li>
+				<li><a href="about.html"> About Us </a></li>
+				<li><a href="index.php"> Dashboard </a></li>
+				<li><a href="contactus.html"> Contact Us </a></li>
+			</ul>
+		</div>
+	</header>
+	<div id="header">
+       <br>
+	   <br>
+	   <br>
+	   <br>
+	   <br>
+       <h2 style=" font-family:Agency FB; font-size: 55px;color:#FFF;text-shadow: 2px 2px black;"> JAMA PUNJI BANK </h2>
+    </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr><br>
+				<a href="getdetail.php">
+               <button class="button1" type="button" href="getdetail.php">MY TRANSACTION</button>
+                </a>
+                </tr>
+				
+			   <tr>        
+               <br> <br> <br>
+			   <a href="transaction.php">
+			   <button class="button2" type="button">TRANSACTION RECORD</button>
+               </a>
+               
+               </tr>
+            </table>
+    </div>
+</body>
+</html>
